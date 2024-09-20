9b9e0dd2cf7b4f555bda6ec5f62ea37dcfdf4c74
Steel Ferguson <steel_ferguson@yahoo.com>
2024-09-20 14:52:44 -0500

    temporarily isolating the app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,14 @@ from dash import Dash
 app = Dash(__name__)
 
 # You can set any server configurations here
+# server = app.server
+
+app.layout = "Hello, World!"
+
 server = app.server
 
-# Optional: Add external stylesheets, meta tags, etc.
-app.title = "Basin Climbing Dashboard"
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+# # Optional: Add external stylesheets, meta tags, etc.
+# app.title = "Basin Climbing Dashboard"
