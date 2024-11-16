cd9139750620e3c5894f996583a12bcb431c0a60
Steel Ferguson <steel_ferguson@yahoo.com>
2024-11-15 22:58:46 -0600

    adding retreat

--- src/pullSquareData.py
+++ src/pullSquareData.py
@@ -14,6 +14,7 @@ class pullSquareData:
         'booking': 'Event Booking',
         'event': 'Event Booking',
         'birthday': 'Event Booking',
+        'retreat': 'Event Booking',
         'pass': 'Day Pass',
         'capitan': 'Day Pass' ## Just for Square
     }
